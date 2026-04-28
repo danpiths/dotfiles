c0dfea0c091032addd942dd88c1d89b3b216de4f
Dan Piths <85949566+danpiths@users.noreply.github.com>
2026-04-28 19:38:51 +0530

    feat: add extra cache via cachix

--- configuration.nix
+++ configuration.nix
@@ -37,6 +37,8 @@ in {
 
   environment.etc."nix/nix.custom.conf".text = ''
     trusted-users = root dhwanil
+    extra-substituters = https://nix-community.cachix.org
+    extra-trusted-public-keys = nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=
   '';
 
   system = {
