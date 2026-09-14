7b1f140f6d5fbed39ff7da5439e6fde659380862
Dan Piths <85949566+danpiths@users.noreply.github.com>
2026-09-14 12:31:28 +0530

    feat: adds --keep-one to nh clean cmd to preserve direnv envs from being fully cleaned up

--- home/programs.nix
+++ home/programs.nix
@@ -19,7 +19,7 @@
   nh = {
     enable = true;
     clean.enable = true;
-    clean.extraArgs = "--keep-since 4d --keep 3";
+    clean.extraArgs = "--keep-since 4d --keep 3 --keep-one";
     darwinFlake = "${homeDirectory}/dotfiles"; # sets NH_OS_FLAKE variable for you
   };
 
